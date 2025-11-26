572bc47ecd5bc5b50b94cddc4438d389644120a3
sylviamuturi7 <sylviamuturi7@gmail.com>
2025-11-26 08:49:48 +0300

    lab3js

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+//function to calculate tax on a given amount
+function calculateTax(amount) {
+    return (amount * 0.1);
+}
+
+//function to convert a string to uppercase
+function convertToUpperCase(str) {
+    return str.toUpperCase();
+}
+
+//function to find the maximum number
+function findMaximum(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+
+//function to check if a string is a palindrome
+function isPalindrome(str) {
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+}
+
+//function to calculate discounted price
+function calculateDiscountedPrice(originalprice, discountPercentage) { 
+ return originalprice - (originalprice * discountPercentage / 100); 
+}
+    
+
+
 
 
 
